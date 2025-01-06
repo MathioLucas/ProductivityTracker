350d93a050237af04a426e6f695a8d95df9404b5
MathioLucas <162763121+MathioLucas@users.noreply.github.com>
2025-01-05 19:56:24 -0500

    Update script.py

--- script.py
+++ script.py
@@ -6,3 +6,13 @@ import tkinter as tk
 from tkinter import messagebox
 import matplotlib.pyplot as plt
 from plyer import notification
+
+class ProductivityTracker:
+    def __init__(self):
+        """Initialize the productivity tracking system"""
+        self.db_connection = sqlite3.connect('productivity.db')
+        self.cursor = self.db_connection.cursor()
+        self.create_tables()
+        self.pomodoro_active = False
+        self.current_task = None
+        self.setup_gui()
